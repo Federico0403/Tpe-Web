427692e4541f56a023928de1edb5dba4cbe766dd
Federico <poupeau04@gmail.com>
2024-10-18 22:52:48 -0300

    arreglo de estetica NOCSS

--- router.php
+++ router.php
@@ -95,7 +95,6 @@ switch ($params[0]) {
         break;
     case 'agregarProductora':
         sessionAuthMiddleware($res);
-        var_dump($res);
         verifyAuthMiddleware($res); 
         $controller = new AdminController($res);
         $controller->addProducer();
